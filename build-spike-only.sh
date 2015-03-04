9036ee4f7597ba3ac0d5f1ac17e4433e161c7983
Isaac Evans <ine@mit.edu>
2015-03-04 17:36:40 -0500

    add commands to build a separate version of spike

--- build-spike-only.sh
+++ build-spike-only.sh
@@ -14,7 +14,20 @@ fi
 echo "Starting RISC-V Toolchain build process"
 
 build_project riscv-fesvr --prefix=$RISCV
+
+
 build_project riscv-isa-sim --prefix=$RISCV --with-fesvr=$RISCV
+# copy the spike output executable, then build it again
+cp $RISCV/bin/spike $RISCV/bin/spike-vanilla
+
+# build with a specific tag policy
+CPPFLAGS="-D TAG_POLICY_NO_RETURN_COPY" CFLAGS="-D TAG_POLICY_NO_RETURN_COPY" build_project riscv-isa-sim --prefix=$RISCV --with-fesvr=$RISCV
+# copy the spike output executable, then build it again
+cp $RISCV/bin/spike-no-return-copy $RISCV/bin/spike-no-return-copy
+
+# build vanilla spike
+build_project riscv-isa-sim --prefix=$RISCV --with-fesvr=$RISCV
+
 CC=riscv64-unknown-elf-gcc build_project riscv-pk --prefix=$RISCV/riscv64-unknown-elf --host=riscv
 
 echo -e "\\nRISC-V Toolchain installation completed!"
