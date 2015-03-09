084661ce42643dd241e7c6f415d2a3a87b4189c6
Julian Gonzalez <jugonz97@mit.edu>
2015-03-08 21:43:11 -0400

    Build now works for machines w/o existing build
    build-spike-only.sh would exit if you didn't already have
    the riscv-unknown-elf-gcc compiler in your $PATH before building.
    Now, build-spike-only.sh will just not build riscv-pk
    if the riscv-unknown-elf-gcc compiler is not present.

--- build-spike-only.sh
+++ build-spike-only.sh
@@ -5,16 +5,9 @@
 
 . build.common
 
-if [ ! `which riscv64-unknown-elf-gcc` ]
-then
-  echo "riscv64-unknown-elf-gcc doesn't appear to be installed; use the full-on build.sh"
-  exit 1
-fi
-
 echo "Starting RISC-V Toolchain build process"
 
 build_project riscv-fesvr --prefix=$RISCV
-
 
 build_project riscv-isa-sim --prefix=$RISCV --with-fesvr=$RISCV
 # copy the spike output executable, then build it again
@@ -27,6 +20,14 @@ cp $RISCV/bin/spike $RISCV/bin/spike-no-return-copy
 
 # build vanilla spike
 build_project riscv-isa-sim --prefix=$RISCV --with-fesvr=$RISCV
+
+# don't build riscv-pk if we don't have the correct cross-compiler
+# note that this depends on having riscv64-unknown-elf-gcc in your $PATH
+if [ ! `which riscv64-unknown-elf-gcc` ]
+then
+  echo "riscv64-unknown-elf-gcc doesn't appear to be installed; not building riscv-pk"
+  exit 1
+fi
 
 CC=riscv64-unknown-elf-gcc build_project riscv-pk --prefix=$RISCV/riscv64-unknown-elf --host=riscv
 
